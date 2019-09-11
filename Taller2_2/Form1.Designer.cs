b7b9567567bdbe07171acb3b6a91cc326521ccc2
Carlos Arango Betancurt <carlos.arango.b.2019@gmail.com>
2019-09-10 22:26:28 -0500

    Crear un programa que solicite el año de nacimiento Con base al año actual deberá calcular los años de vida

---
+++ Taller2_2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Taller2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnacimiento = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblaños = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Año nacimiento";
+            // 
+            // txtnacimiento
+            // 
+            this.txtnacimiento.Location = new System.Drawing.Point(151, 41);
+            this.txtnacimiento.Name = "txtnacimiento";
+            this.txtnacimiento.Size = new System.Drawing.Size(100, 20);
+            this.txtnacimiento.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblaños
+            // 
+            this.lblaños.AutoSize = true;
+            this.lblaños.Location = new System.Drawing.Point(49, 135);
+            this.lblaños.Name = "lblaños";
+            this.lblaños.Size = new System.Drawing.Size(35, 13);
+            this.lblaños.TabIndex = 3;
+            this.lblaños.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblaños);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtnacimiento);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnacimiento;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblaños;
+    }
+}
+
---
+++ Taller2_2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Taller2_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int años= (2019 - Convert.ToInt16(txtnacimiento.Text));
+            lblaños.Text = años.ToString();
+        }
+    }
+}
